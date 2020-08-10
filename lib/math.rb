45d7d1ffd9b1d2a4b883c596ba1746dd7fd50b18
Kevin Botero <kbotero201@gmail.com>
2020-08-10 18:25:15 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,36 @@
 def addition(num1, num2)
-
+sum = 5 + 4
+puts sum
+sum
 end
 
-def subtraction(num1, num2)
 
+def subtraction(num1, num2)
+  sum = 10 - 5
+  puts sum
+  sum
 end
 
 def division(num1, num2)
-
+  sum = 50 / 2
+  puts sum
+  sum
 end
 
 def multiplication(num1, num2)
-
+  sum = 4 * 30
+  puts sum
+  sum
 end
 
 def modulo(num1, num2)
-
+  remainder = 34 % 5
+  puts remainder
+  remainder
 end
 
 def square_root(num)
-
+  square = Math.sqrt(81)
+  puts square
+  square
 end
