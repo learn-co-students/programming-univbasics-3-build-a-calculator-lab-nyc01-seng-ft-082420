66d5a65a8008692649df2f9f698a7bfa7fa057f1
Darkwah Boahen (Sam) <sdboahen@gmail.com>
2020-08-15 21:57:35 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,90 @@
-def addition(num1, num2)
+# Calculators can be very useful devices in day-to-day activities. 
+# You've likely used a calculator to add up bills for this month or--// . . . O_O . . . //
 
+
+
+
+def 
+  
+  addition(num1, num2)
+
+
+  sum = (num1 + num2)
+  
+  sum
+  
 end
 
-def subtraction(num1, num2)
 
+
+
+def 
+  
+  subtraction(num1, num2)
+
+
+  difference = (num1 - num2)
+  
+  difference
+  
 end
 
-def division(num1, num2)
 
+
+
+def 
+  
+  division(num1, num2)
+
+
+  quotient = (num1/num2)
+  
+  quotient
+  
 end
 
-def multiplication(num1, num2)
 
+
+
+def 
+  
+  multiplication(num1, num2)
+
+
+  product = (num1 * num2)
+  
+  product
+  
 end
 
-def modulo(num1, num2)
 
+
+
+def 
+  
+  modulo(num1, num2)
+
+
+  moduquo = (num1%num2)
+  #modu_quo
+  
+  moduquo
+  
 end
 
-def square_root(num)
 
+
+
+def 
+  
+  square_root(num)
+
+
+  sq_rt = Math.sqrt(num)
+  
+  sq_rt
+  
 end
+
+
+
